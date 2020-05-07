a4dc5f4402293e9bcb7b685fe5e6f585586a5a40
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-05-08 04:10:38 +0530

    Tkinter : Front end fake demo done
    Signed-off-by: Aditya Srivastava <adityasrivastava301199@gmail.com>

--- app.py
+++ app.py
@@ -3,29 +3,37 @@ from PIL import ImageTk, Image
 import model
 import convert
 from tkinter.filedialog import askopenfile
+import pandas as pd
+
+
 def go_back(window):
     window.destroy()
     main()
+
 def get_input_screen(window):
     window.destroy()
     window2 = Tk()
     window2.title("Think2Type")
     window2.geometry('600x300')
     window2.configure(background='white')
+
     btn = Button(window2, text="Back", width=4, height=1, 
-                 borderwidth= 2, bg="white", relief="solid",
+                 borderwidth= 1, bg="white", relief="solid",
                  command=lambda: go_back(window2))
     btn.place(relx=0.05, rely=0.10, anchor=CENTER)
+
     # pixelVirtual = PhotoImage(width=10, height=1)
     # img = ImageTk.PhotoImage(Image.open("img/Logo.jpeg"))
     # panel = Label(window2, image = img)
     # panel.place(relx=0.5, rely=0.05, anchor=N)
+
     v = StringVar(window2, "1") 
     values = {"Device Connected" : 1, 
               "Collecting Data" : 2, 
               "Predicting Thought" : 3, 
               "Morse Code Generated" : 4, 
-              "Text Generated" : 5} 
+              "Text Generated" : 5}
+
     for (text, value) in values.items(): 
         rbtn = Radiobutton(window2, text = text, variable=v,
                            value=value)
@@ -38,10 +46,24 @@ def get_input_screen(window):
     pasw = Entry(window2)
     pasw.place(relx = 0.3, rely=0.75)
 
-def get_file():
-    f = askopenfile(mode ='r', filetypes =['*.csv', '*.txt'])
+def get_file_fill_password(pasw_field):
+    f = askopenfile(mode ='r', filetypes =[('Comma Separated Values','*.csv'), ('Text Files','*.txt')])
     if f is not None:
-        print('dgk')
+        path = f.name
+        df = pd.read_csv(path) #assuming csv file...
+        data = df.values       ## size expected : (x,64)
+                               ## x == length of the ^(pass)?word$
+
+        # TODO : Make a file we can use ---> priority very high
+        data = [] #till we dont get actual file to use
+        password = ''
+        for sequence in data: 
+            morse_code = model.predict(sequence)
+            character = convert.morse_to_text(morse_code)
+            password += character
+        if password == '':
+            password = 'test'
+        pasw_field.insert(0, password)
 
 def upload_file(window):
     window.destroy()
@@ -53,29 +75,25 @@ def upload_file(window):
     ## TODO: display the passoword in a text box ---> priority high
     ## TODO: Fix logo issue ---> priority low
 
+    label = Label(window2, text='Password')
+    label.place(relx = 0.25, rely =0.75)
+
+    pasw_field = Entry(window2)
+    pasw_field.place(relx = 0.40, rely=0.75)
+
+    btn = Button(window2, text="Back", width=4, height=1, 
+                 borderwidth= 1, bg="white", relief="solid",
+                 command=lambda: go_back(window2))
+    btn.place(relx=0.05, rely=0.10, anchor=CENTER)
+
     # img2 = ImageTk.PhotoImage(Image.open("./img/Logo.jpeg"))
     # panel2 = Label(window2, image = img2)
     # panel2.place(relx=0.5, rely=0.05, anchor=N)
 
     btn = Button(window2, text="Upload", width=20, height=4, 
                  borderwidth= 2, bg="white", relief="solid",
-                 command=lambda: get_file())
+                 command=lambda: get_file_fill_password(pasw_field))
     btn.place(relx=0.5, rely=0.35, anchor=CENTER)
-    
-    ## assuming I got the data...
-    ## size expected : (x,64)
-    ## x == length of the ^(pass)?word$
-
-    data = []
-    password = ''
-    for sequence in data: 
-        morse_code = model.predict(sequence)
-        character = convert.morse_to_text(morse_code)
-        password += character
-    if password == '':
-        password = 'test'
-    
-
 
 def main():
     window = Tk()
@@ -83,7 +101,7 @@ def main():
     window.geometry('600x300')
     window.configure(background='white')
 
-    pixelVirtual = PhotoImage(width=10, height=1)
+    # pixelVirtual = PhotoImage(width=10, height=1)
     img = ImageTk.PhotoImage(Image.open("img/Logo.jpeg"))
     panel = Label(window, image = img)
     panel.place(relx=0.5, rely=0.05, anchor=N)
